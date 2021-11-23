d9829d43a247f2e408c9a68b514f2f699cf273c7
ckanelin <lincandy.yite@gmail.com>
2021-11-23 00:49:12 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import FakeComponent from "./FakeComponent";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <FakeComponent name="Candy" colour="red" />
       </header>
     </div>
   );
---
+++ src/FakeComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FakeComponent = ({ name, colour }) => {
+  return <div style={{ backgroundColor: colour }}> {name} Component</div>;
+};
+
+export default FakeComponent;
