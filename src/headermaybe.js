d446dc3a02983feeb53bf2b42a4aa62f6c0aa488
gasparf <gaspar.tan1189@gmail.com>
2021-11-23 14:53:11 -0800

    testing

--- src/headermaybe.js
+++ src/headermaybe.js
@@ -1,16 +1,10 @@
 import React from "react";
 
-const container = <div style="width:50" />
-const iconSize = <div style="width:50,height:50" />
+
 
 const DispIcon = () => {
-    return (
-        <container>
-            <iconsize>
-            <img src="https://i.picsum.photos/id/703/50/50.jpg?hmac=JQ5TMMUcJ7cUTgc7_GPt8sfLkiXC-iFpcEoZktg9Nzg" />
-            </iconsize>
-        </container>
-    )
+    return ( < img alt = "testing icon"
+        src = "https://i.picsum.photos/id/703/50/50.jpg?hmac=JQ5TMMUcJ7cUTgc7_GPt8sfLkiXC-iFpcEoZktg9Nzg" / > )
 }
 
 export default DispIcon;
