ef46aaf4333dfbe3cbb0c0f93227b851baf2b581
Gmonay <gaspar.tan@github.com>
2021-11-23 01:39:04 -0800

    testing icon addition

--- src/headermaybe.js
+++ src/headermaybe.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const container = <div style="width:50" />
-const iconSize = <div style="width:5,height:50" />
+const iconSize = <div style="width:50,height:50" />
 
 const DispIcon = () => {
     return (
         <container>
-            <iconsize
-            source={{uri:'https://i.picsum.photos/id/703/50/50.jpg?hmac=JQ5TMMUcJ7cUTgc7_GPt8sfLkiXC-iFpcEoZktg9Nzg'}} />
+            <iconsize>
+            <img src="https://i.picsum.photos/id/703/50/50.jpg?hmac=JQ5TMMUcJ7cUTgc7_GPt8sfLkiXC-iFpcEoZktg9Nzg" />
+            </iconsize>
         </container>
     )
 }
