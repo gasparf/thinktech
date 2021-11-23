3ad7ebe553e0b5401f00c86157505fa890443a5f
gasparf <gaspar.tan1189@gmail.com>
2021-11-23 15:38:28 -0800

    1

--- src/App.js
+++ src/App.js
@@ -4,27 +4,32 @@ import FakeComponent from "./FakeComponent";
 import DispIcon from "./headermaybe";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <DispIcon/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <FakeComponent name="Candy" colour="red" />
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
 
-      </header>
-    </div>
-  );
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Edit < code > src / App.js < /code> and save to reload. <
+        /p> <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> <
+        FakeComponent name = "Candy"
+        colour = "red" / >
+
+        <
+        /header> <
+        /div>
+    );
 }
 
 export default App;
--- src/navbar.js
+++ src/navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 
 
-const DispIcon = () => {
+const sideBar = () => {
     return ( < img alt = "testing icon"
         src = "https://i.picsum.photos/id/703/50/50.jpg?hmac=JQ5TMMUcJ7cUTgc7_GPt8sfLkiXC-iFpcEoZktg9Nzg" / > )
-}
+};
 
 export default DispIcon;
