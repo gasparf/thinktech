238ac78f683762138b47d2397a335c76028f0d0b
ckanelin <lincandy.yite@gmail.com>
2021-11-24 01:19:38 -0800

    add mortage loan page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Home from "./testPages/Home";
 import Accounts from "./testPages/Accounts";
 import Borrowing from "./testPages/Borrowing";
-
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
+import MortgageLoans from "./MortgageLoans/MortgageLoans";
 
 function App() {
-    return ( 
-        <>
-        <Router>
-            <div className='App'>
-                <NavBar/>
-                <Routes>
-                    <Route path="/" exact element={<Home />}/>
-                    <Route path="/accounts" exact element={<Accounts />}/>
-                    <Route path="/borrowing" exact element={<Borrowing />}/>
-                </Routes>
-                
-
-            </div>
-
-        </Router>
-           
-        </>
-         
-       
-    );
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/accounts" exact element={<Accounts />} />
+            <Route path="/borrowing" exact element={<MortgageLoans />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
