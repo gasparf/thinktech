f2fc8a78969574979915ca64abd5076e587ee39c
Gmonay <gaspar.tan@github.com>
2021-11-24 19:33:20 -0800

    table init

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,21 @@
 import React, {useState} from 'react';
 import "./Home.css";
 import Graph from "../HomeGraph/Graph";
+import Table from "../HomeTable/Table";
 
 
 const Home = () => {
     return (
         <div className='home'>
             
-                <Graph/>
-                
+
+            {/* add seperate div for the top row? */}
+
+            <Graph/>
+            
+            <div className='bottomRow'>
+                <Table />
+            </div>    
         
         
         </div>
---
+++ src/HomeTable/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Table.css';
+
+function Table() {
+    return (
+        <div className='table'>
+            
+        </div>
+    )
+}
+
+export default Table
